ade46cfa95e7fffe0bb0bc50310968954e1a5540
alex <xandergamer79@gmail.com>
2026-07-28 17:57:46 -0600

    Add function to create movie sections dynamically

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ function filtrar(categoria){
 
     if(categoria === "Todas"){
 
-        mostrarPeliculas(peliculas);
+     crearSecciones();
 
         return;
 
@@ -138,6 +138,199 @@ if(buscador){
 
 
     });
+
+
+}
+function crearSecciones(){
+
+
+const contenedor =
+document.getElementById("secciones-cine");
+
+
+contenedor.innerHTML="";
+
+
+
+const categorias = [
+
+
+{
+nombre:"🔥 Más populares",
+filtro:"Todas"
+},
+
+
+{
+nombre:"🚀 Ciencia ficción",
+filtro:"Ciencia ficción"
+},
+
+
+{
+nombre:"👻 Terror",
+filtro:"Terror"
+},
+
+
+{
+nombre:"🎌 Anime",
+filtro:"Anime"
+},
+
+
+{
+nombre:"💥 Acción",
+filtro:"Acción"
+}
+
+
+];
+
+
+
+
+
+categorias.forEach(categoria=>{
+
+
+let lista;
+
+
+
+if(categoria.filtro==="Todas"){
+
+
+lista = peliculas;
+
+
+}else{
+
+
+lista = peliculas.filter(
+
+pelicula =>
+pelicula.genero === categoria.filtro
+
+);
+
+
+}
+
+
+
+
+if(lista.length > 0){
+
+
+
+let bloque = document.createElement("div");
+
+
+bloque.className="seccion-peliculas";
+
+
+
+bloque.innerHTML=`
+
+
+<h2>
+
+${categoria.nombre}
+
+</h2>
+
+
+<div class="catalogo">
+
+
+</div>
+
+
+`;
+
+
+
+let catalogoSeccion =
+bloque.querySelector(".catalogo");
+
+
+
+
+
+lista.forEach((pelicula)=>{
+
+
+let tarjeta=document.createElement("div");
+
+
+tarjeta.className="card";
+
+
+
+tarjeta.innerHTML=`
+
+
+<img src="${pelicula.imagen}">
+
+
+<h3>
+${pelicula.titulo}
+</h3>
+
+
+<p>
+${pelicula.genero} | ${pelicula.año}
+</p>
+
+
+<button>
+Ver detalles
+</button>
+
+
+`;
+
+
+
+
+
+tarjeta.querySelector("button")
+.onclick=function(){
+
+
+let id =
+peliculas.indexOf(pelicula);
+
+
+
+window.location.href =
+"detalles.html?id="+id;
+
+
+};
+
+
+
+catalogoSeccion.appendChild(tarjeta);
+
+
+
+});
+
+
+
+
+contenedor.appendChild(bloque);
+
+
+
+}
+
+
+
+});
+
 
 
 }
