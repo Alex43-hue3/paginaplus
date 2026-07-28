637ed423623f10c1b49918ea627474a64892956b
alex <xandergamer79@gmail.com>
2026-07-28 16:43:27 -0600

    Add movie list with details in peliculas.js

--- peliculas.js
+++ peliculas.js
@@ -1,1 +1,49 @@
+const peliculas=[
 
+
+{
+titulo:"Interestelar",
+genero:"Ciencia ficción",
+año:"2014",
+imagen:"https://image.tmdb.org/t/p/w500/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg",
+descripcion:"Un viaje espacial buscando un nuevo hogar para la humanidad."
+},
+
+
+{
+titulo:"Batman: El Caballero de la Noche",
+genero:"Acción",
+año:"2008",
+imagen:"https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
+descripcion:"Batman enfrenta a uno de sus mayores enemigos."
+},
+
+
+{
+titulo:"Avatar",
+genero:"Ciencia ficción",
+año:"2009",
+imagen:"https://image.tmdb.org/t/p/w500/jRXYjXNq0Cs2TcJjLkki24MLp7u.jpg",
+descripcion:"Una aventura en el planeta Pandora."
+},
+
+
+{
+titulo:"El Conjuro",
+genero:"Terror",
+año:"2013",
+imagen:"https://image.tmdb.org/t/p/w500/wVYREutTvI2tmxr6ujrHT705wGF.jpg",
+descripcion:"Una familia enfrenta una presencia sobrenatural."
+},
+
+
+{
+titulo:"Akira",
+genero:"Anime",
+año:"1988",
+imagen:"https://image.tmdb.org/t/p/w500/neZ0ykEsPqxamsX6o5QNUFILQ0.jpg",
+descripcion:"Un clásico del anime japonés."
+}
+
+
+];
