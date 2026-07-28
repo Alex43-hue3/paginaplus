6093643e6377499db6f744be7db1b632e7ad848f
alex <xandergamer79@gmail.com>
2026-07-28 17:47:37 -0600

    Add functionality to display movie banner details

--- banner.js
+++ banner.js
@@ -1,1 +1,59 @@
 
+const peliculaBanner = peliculas[0];
+
+
+const banner = document.getElementById("banner");
+
+
+banner.style.backgroundImage = `
+
+linear-gradient(
+to right,
+#09090d,
+transparent
+),
+
+url(${peliculaBanner.imagen})
+
+`;
+
+
+
+document.getElementById("titulo-banner").textContent =
+peliculaBanner.titulo;
+
+
+
+document.getElementById("rating-banner").textContent =
+"⭐ " + (peliculaBanner.rating || "8.5");
+
+
+
+document.getElementById("genero-banner").textContent =
+"🎬 " + peliculaBanner.genero;
+
+
+
+document.getElementById("año-banner").textContent =
+"📅 " + peliculaBanner.año;
+
+
+
+document.getElementById("descripcion-banner").textContent =
+peliculaBanner.descripcion;
+
+
+
+
+document.getElementById("boton-banner")
+.onclick=function(){
+
+
+let id = peliculas.indexOf(peliculaBanner);
+
+
+window.location.href =
+"detalles.html?id="+id;
+
+
+};
