e9427744da89259be8e3097ef9ffbc6932914975
alex <xandergamer79@gmail.com>
2026-07-28 16:48:31 -0600

    Implement movie display and filtering functionality

--- script.js
+++ script.js
@@ -1,1 +1,102 @@
 
+const catalogo=document.getElementById("catalogo");
+
+
+function mostrarPeliculas(lista){
+
+
+catalogo.innerHTML="";
+
+
+lista.forEach(p=>{
+
+
+catalogo.innerHTML+=`
+
+<div class="card">
+
+
+<img src="${p.imagen}">
+
+
+<h3>${p.titulo}</h3>
+
+
+<p>
+${p.genero} | ${p.año}
+</p>
+
+
+<p>
+${p.descripcion}
+</p>
+
+
+</div>
+
+
+`;
+
+
+});
+
+
+}
+
+
+
+mostrarPeliculas(peliculas);
+
+
+
+
+
+function filtrar(categoria){
+
+
+if(categoria=="Todas"){
+
+mostrarPeliculas(peliculas);
+
+return;
+
+}
+
+
+
+let resultado=
+peliculas.filter(
+p=>p.genero==categoria
+);
+
+
+mostrarPeliculas(resultado);
+
+
+}
+
+
+
+
+document
+.getElementById("buscar")
+.addEventListener(
+"input",
+function(){
+
+
+let texto=this.value.toLowerCase();
+
+
+let resultado=
+peliculas.filter(
+p=>
+p.titulo.toLowerCase()
+.includes(texto)
+);
+
+
+mostrarPeliculas(resultado);
+
+
+});
