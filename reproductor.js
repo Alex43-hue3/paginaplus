d33ab09ed6dc77b55196c4e84019e4383c7b14ef
alex <xandergamer79@gmail.com>
2026-07-29 00:19:25 -0600

    Add movie details display in reproductor.js

--- reproductor.js
+++ reproductor.js
@@ -1,1 +1,75 @@
 
+const parametros =
+new URLSearchParams(
+window.location.search
+);
+
+
+
+const id =
+parametros.get("id");
+
+
+
+const pelicula =
+peliculas[id];
+
+
+
+
+const video =
+document.getElementById("video");
+
+
+
+
+if(pelicula){
+
+
+
+document.getElementById("titulo")
+.textContent =
+pelicula.titulo;
+
+
+
+
+document.getElementById("info")
+.textContent =
+pelicula.genero +
+" | " +
+pelicula.año;
+
+
+
+
+
+video.src =
+pelicula.video;
+
+
+
+
+document.getElementById("volver")
+.onclick=function(){
+
+
+window.location.href =
+"detalles.html?id="+id;
+
+
+};
+
+
+
+
+}
+else{
+
+
+document.getElementById("titulo")
+.textContent =
+"Película no encontrada";
+
+
+}
