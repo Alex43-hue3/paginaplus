e13edd27c6f1b14e7790a319b745b79a29530a5b
alex <xandergamer79@gmail.com>
2026-07-29 00:15:01 -0600

    Display movie details based on URL parameters

--- detalles.js
+++ detalles.js
@@ -1,1 +1,107 @@
 
+const parametros =
+new URLSearchParams(
+window.location.search
+);
+
+
+
+const id =
+parametros.get("id");
+
+
+
+const pelicula =
+peliculas[id];
+
+
+
+if(pelicula){
+
+
+
+document.getElementById("titulo")
+.textContent =
+pelicula.titulo;
+
+
+
+
+document.getElementById("poster")
+.src =
+pelicula.imagen;
+
+
+
+
+document.getElementById("fondo")
+.style.backgroundImage =
+`url(${pelicula.imagen})`;
+
+
+
+
+
+document.getElementById("rating")
+.textContent =
+"⭐ " + pelicula.rating;
+
+
+
+
+document.getElementById("genero")
+.textContent =
+"🎬 " + pelicula.genero;
+
+
+
+
+document.getElementById("año")
+.textContent =
+"📅 " + pelicula.año;
+
+
+
+
+document.getElementById("descripcion")
+.textContent =
+pelicula.descripcion;
+
+
+
+
+
+
+
+document.getElementById("trailer")
+.onclick=function(){
+
+
+window.open(
+pelicula.trailer,
+"_blank"
+);
+
+
+};
+
+
+
+
+
+
+
+document.getElementById("pelicula")
+.onclick=function(){
+
+
+window.location.href =
+"reproductor.html?id="+id;
+
+
+};
+
+
+
+
+}
